3af6aea535c15f2792b7bfdb95f01aeca5d0675a
Dhanashree Balaji <dhanashreeblj@gmail.com>
2024-02-15 19:16:34 +0530

    Trip Plan React App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+
+import data from './Components/data'
+import Tours from './Components/Tours';
+
 
 function App() {
+  
+ const[tourdetails , setTours] = useState(data);
+ //console.log(tourdetails);
+
+//Removing not interested tours from UI. tourdetails varaible is re filled
+   function removeTour(id){
+    const newTours = tourdetails.filter(tour => tour.id !== id);
+    setTours(newTours);
+   }
+  
+ //If all tours are removed from The list in UI, show this code
+   if(tourdetails.length === 0){
+    return(
+      <div className="refresh">
+      <h2>No Tours left</h2>
+     <button onClick={ () => setTours(data)}>
+ 
+      Refresh
+     </button>
+      </div>
+    );
+   }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Tours tours = {tourdetails} removeTour={removeTour}> </Tours>
+   </div>
   );
 }
 
